5aee1920d613369fac3471788d7973cce9bd39bf
redhawk <972419185@qq.com>
2019-11-27 10:09:43 +0800

    添加说明,添加依賴;以及空配置类; idea的配置文件不要上传上去

--- HELP.md
+++ HELP.md
@@ -5,12 +5,16 @@ By: redhawk email:972419185@qq.com
 SpringBoot2.x
 MyBatisPlus3.x
 MySQL 5.7
-SpringSecurity + OAuth2 + JWT
-Vue 2.x
+SpringSecurity5 + OAuth2 + JWT
 Hutool5       <!--  文档地址 CTRL+鼠标左键直接跳转连接 https://www.hutool.club/docs/#/  -->
-Swagger2
+Swagger2 接口总汇,支持crud测试
 Lombok
+Elasticsearch7.x 博客搜索功能
+WebJars 使用webjars来管理静态资源
 Thymeleaf
+Vue 2.x
+Vue axios
+ElementUi
 #开发规范
 遵守1688开发手册为前提
 #版本迭代
---
+++ src/main/java/xyz/redhawk/kapok/config/security/SpringSecurityConfig.java
@@ -0,0 +1,21 @@
+package xyz.redhawk.kapok.config.security;/**
+ * @author RedHawk
+ * @create 2019-11-27 9:25
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * <Description> <br>
+ *
+ * @author redhawk<br>
+ * @taskId:   <br>
+ * @version 1.0<br>
+ * @createDate 2019/11/27 9:25 <br>
+ * @see xyz.redhawk.kapok.config.security <br>
+ */
+public class SpringSecurityConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringSecurityConfig.class);
+}
